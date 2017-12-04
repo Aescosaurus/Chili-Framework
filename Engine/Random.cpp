21cfff4121a869839b69703cf00f0fb17c87402b
RadAesc <michaelbuslik@hotmail.com>
2017-12-03 18:08:43 -0700

    Implemented NextFloat method in Random.

--- Engine/Random.cpp
+++ Engine/Random.cpp
@@ -11,3 +11,9 @@ int Random::NextInt( int min,int max )
 	std::uniform_int_distribution<int> dist( min,max );
 	return dist( rng );
 }
+
+float Random::NextFloat( float min,float max )
+{
+	std::uniform_real_distribution<float> dist( min,max );
+	return dist( rng );
+}
--- Engine/Random.h
+++ Engine/Random.h
@@ -7,6 +7,7 @@ class Random
 public:
 	Random();
 	int NextInt( int min,int max );
+	float NextFloat( float min,float max );
 private:
 	std::mt19937 rng;
 };
