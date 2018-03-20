f1519c4beabe2975a605630e21ad423034548025
RadAesc <michaelbuslik@hotmail.com>
2018-03-19 22:04:27 -0600

    Const-ified Random methods.

--- Engine/Random.cpp
+++ Engine/Random.cpp
@@ -6,14 +6,26 @@ Random::Random()
 {
 }
 
-int Random::NextInt( int min,int max )
+int Random::RangeI( int min,int max ) const
 {
 	std::uniform_int_distribution<int> dist( min,max );
 	return dist( rng );
 }
 
-float Random::NextFloat( float min,float max )
+float Random::RangeF( float min,float max ) const
 {
 	std::uniform_real_distribution<float> dist( min,max );
 	return dist( rng );
 }
+
+// int Random::NextInt( int min,int max )
+// {
+// 	std::uniform_int_distribution<int> dist( min,max );
+// 	return dist( rng );
+// }
+// 
+// float Random::NextFloat( float min,float max )
+// {
+// 	std::uniform_real_distribution<float> dist( min,max );
+// 	return dist( rng );
+// }
